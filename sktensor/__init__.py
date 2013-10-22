c27d19eb006166bec9da73b10d5977dd8b843ea2
Maximilian Nickel <nickel@dbs.ifi.lmu.de>
2013-10-22 16:44:01 -0400

    import ktensor

--- sktensor/__init__.py
+++ sktensor/__init__.py
@@ -2,4 +2,6 @@ from version import __version__
 
 from utils import *
 from core import *
-from sptensor import *
+from sptensor import sptensor, unfolded_sptensor
+from dtensor import dtensor, unfolded_dtensor
+from ktensor import ktensor
