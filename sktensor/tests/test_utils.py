140704e30daefdb7450cdc3779e29a94164b066d
Maximilian Nickel <nickel@dbs.ifi.lmu.de>
2013-12-09 13:23:03 -0500

    check accum for a few more indices

--- sktensor/tests/test_utils.py
+++ sktensor/tests/test_utils.py
@@ -4,13 +4,13 @@ from numpy import array, allclose
 
 
 def test_accum():
-    subs1 = array([0, 1, 1])
-    subs2 = array([0, 1, 1])
-    vals = array([1, 2, 3])
+    subs1 = array([0, 1, 1, 2, 2, 2])
+    subs2 = array([0, 1, 1, 1, 2, 2])
+    vals = array([1, 2, 3, 4, 5, 6])
     nvals, nsubs = accum((subs1, subs2), vals, with_subs=True)
-    assert_true(allclose(nvals, array([1, 5])))
-    assert_true(allclose(nsubs[0], array([0, 1])))
-    assert_true(allclose(nsubs[1], array([0, 1])))
+    assert_true(allclose(nvals, array([1, 5, 4, 11])))
+    assert_true(allclose(nsubs[0], array([0, 1, 2, 2])))
+    assert_true(allclose(nsubs[1], array([0, 1, 1, 2])))
 
     subs1 = array([0, 0, 1])
     subs2 = array([0, 0, 1])
